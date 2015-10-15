ba75321904da10433566b588fc860802fefc7e2b
Ben Shanks <bshanks@unc.edu>
2015-10-15 15:11:09 -0400

    pymc2 position fitting

---
+++ bayes_changepoint/fit_position.py
@@ -0,0 +1,209 @@
+#!/usr/local/bin/python
+from ROOT import *
+TROOT.gApplication.ExecuteFile("$MGDODIR/Root/LoadMGDOClasses.C")
+TROOT.gApplication.ExecuteFile("$MGDODIR/Majorana/LoadMGDOMJClasses.C")
+
+import sys, os
+#import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pymc
+import signal_model_mc2 as sm
+
+
+gatDataName = "mjd_run"
+gatTreeName = "mjdTree"
+builtDataName = "OR_run"
+builtTreeName = "MGTree"
+dataSetName = "surfmjd"
+detectorName = "P3JDY"
+flatTimeSamples = 800
+channelNumber = 688
+
+####################################################################################################################################################################
+
+
+def main(argv):
+  runRange = (6000,6360)
+
+  plt.ion()
+  fig = plt.figure(1)
+  fig2=None
+#  fig2 = plt.figure(2)
+
+  #Instantiate and prepare the baseline remover transform:
+  baseline = MGWFBaselineRemover()
+  baseline.SetBaselineSamples(flatTimeSamples)
+
+
+  #i do this one run at a time, instead of in a chain, because it makes it easier when we want to run on large data sets and create skim files for each run
+  for iRun in range( runRange[0],  runRange[1]+1):
+    print 'processing run', iRun
+    gatFilePath =  os.path.expandvars("$MJDDATADIR/%s/data/gatified/%s/%s%d.root" % (dataSetName, detectorName, gatDataName, iRun  ) )
+    builtFilePath =  os.path.expandvars("$MJDDATADIR/%s/data/built/%s/%s%d.root" % (dataSetName, detectorName, builtDataName, iRun  ) )
+    
+    if not os.path.isfile(gatFilePath):
+      print ">>>Skipping file " + gatFilePath
+      continue
+  
+    gat_file = TFile(gatFilePath)
+    gatTree = gat_file.Get(gatTreeName)
+    built_file = TFile(builtFilePath)
+    builtTree = built_file.Get(builtTreeName)
+    
+    builtTree.AddFriend(gatTree)
+    
+    chanCut =  "channel == %d" % channelNumber
+    energyCut = "trapECal>%f && trapECal<%f" % (1588,1594)
+    
+    cut = energyCut + " && " + chanCut
+    #print "The cuts will be: " + cut
+
+    gatTree.SetEntryList(0)
+    gatTree.Draw(">>elist", cut, "entrylist")
+    elist = gDirectory.Get("elist")
+    #print "Number of entries in the entryList is " + str(elist.GetN())
+    gatTree.SetEntryList(elist);
+    builtTree.SetEntryList(elist);
+    
+    for ientry in xrange( elist.GetN() ):
+      entryNumber = gatTree.GetEntryNumber(ientry);
+      waveform = getWaveform(gatTree, builtTree, entryNumber, channelNumber)
+
+      #there are goofy things that happen at the end of the waveform for mod 1 data because of the presumming.  Just kill the last 5 samples
+      waveform.SetLength(waveform.GetLength()-5)
+      
+      #for now, baseline subtract it here (should eventually be incorporated into the model.  won't be hard.  just lazy.)
+      baseline.TransformInPlace(waveform)
+
+      #MCMC fit and plot the results
+      fitWaveform(waveform, fig, fig2)
+
+
+####################################################################################################################################################################
+
+def getWaveform(gatTree, builtTree, entryNumber, channelNumber):
+    
+    builtTree.GetEntry( entryNumber )
+    gatTree.GetEntry( entryNumber )
+    
+    event = builtTree.event
+    channelVec   = gatTree.channel
+    numWaveforms = event.GetNWaveforms()
+
+    for i_wfm in xrange( numWaveforms ):
+        channel = channelVec[i_wfm]
+        if (channel != channelNumber): continue
+        return event.GetWaveform(i_wfm)
+
+####################################################################################################################################################################
+    
+        
+def fitWaveform(wf, wfFig, zoomFig):
+
+  np_data = wf.GetVectorData()
+  
+  wfMax = np.amax(np_data)
+  
+  #perform the fit up to this index.  Currently set by 99% timepoint (no real point in fitting on the falling edge)
+  lastFitSampleIdx = 1060#findTimePoint(np_data, 0.99)
+  
+  fitSamples = 110 # 1 microsecond
+  
+  firstFitSampleIdx = lastFitSampleIdx - fitSamples
+  
+  np_data_early = np_data[firstFitSampleIdx:lastFitSampleIdx]
+  
+  t0_guess = 40
+  siggen_model = pymc.Model( sm.createSignalModelSiggen(np_data_early, t0_guess, wfMax) )
+ 
+  M = pymc.MCMC(siggen_model)
+  M.use_step_method(pymc.AdaptiveMetropolis, [M.radEst, M.zEst, M.phiEst, M.wfScale], delay=1000)
+  M.use_step_method(pymc.DiscreteMetropolis, M.switchpoint, proposal_distribution='Normal', proposal_sd=4)
+  M.sample(iter=50000)
+ 
+  t0 = t0_guess#M.trace('t0Est')[-1]
+  r = M.trace('radEst')[-1]
+  z = M.trace('zEst')[-1]
+  phi = M.trace('phiEst')[-1]
+  scale = M.trace('wfScale')[-1]
+  startVal = t0_guess + firstFitSampleIdx
+  
+  # Two subplots, the axes array is 1-d
+  f, axarr = plt.subplots(5, sharex=True)
+  
+  axarr[0].plot(M.trace('radEst')[:])
+  axarr[1].plot(M.trace('zEst')[:])
+  axarr[2].plot(M.trace('phiEst')[:])
+  axarr[3].plot(M.trace('wfScale')[:])
+  axarr[4].plot(M.trace('switchpoint')[:])
+
+  
+  print ">>> t0guess was %d" % (firstFitSampleIdx+t0_guess)
+  print ">>> fit t0 was %d" % (firstFitSampleIdx + t0)
+  
+  siggen_fit = sm.findSiggenWaveform(r, phi, z)
+  
+  siggen_fit *= scale
+  
+  #plotting
+
+  plt.figure(wfFig.number)
+  plt.clf()
+  plt.title("Charge waveform")
+  plt.plot( np_data  ,color="red" )
+  plt.xlim( lastFitSampleIdx - fitSamples, lastFitSampleIdx+25)
+  #plt.ylim(-0.1, 2.5)
+  plt.plot(np.arange(startVal, 800+startVal), siggen_fit  ,color="blue" )
+#  plt.plot(np.arange(0, startVal), np.zeros(startVal)  ,color="blue" )
+  plt.axvline(x=lastFitSampleIdx, linewidth=1, color='r',linestyle=":")
+  plt.axvline(x=startVal, linewidth=1, color='g',linestyle=":")
+  
+#  plt.figure(zoomFig.number)
+#  plt.clf()
+#  plt.title("Zoom in near the start time")
+#  plt.plot( np_data  ,color="red" )
+#  plt.xlim( startVal-40, startVal+40)
+#  plt.ylim(-10, 50)
+#  plt.plot(np.arange(startVal, 800+startVal), sigFit  ,color="blue" )
+#  plt.plot(np.arange(0, startVal), np.zeros(startVal)  ,color="blue" )
+#  plt.axvline(x=startVal, linewidth=1, color='g',linestyle=":")
+
+
+  value = raw_input('  --> Press q to quit, any other key to continue\n')
+
+  if value == 'q':
+    exit(1)
+
+
+
+def findTimePoint(data, percent):
+  #don't screw up the data, bro
+  int_data = np.copy(data)
+  int_data /= np.amax(int_data)
+  return np.where(np.greater(int_data, percent))[0][0]
+
+#def findSiggenWaveform(x,y,z,normMax):
+#
+#  weightedClusterPosition = TVector3(x, y, z);
+#  clusterWaveform = MGTWaveform();
+#  calcFlag = siggenInst.CalculateWaveform(weightedClusterPosition, clusterWaveform, 1);
+#  
+#  rcint = MGWFRCIntegration()
+#  rcint.SetTimeConstant(rcIntTimeConstant)
+#  rcint.TransformInPlace(clusterWaveform)
+#  
+#  siggen_data = clusterWaveform.GetVectorData()
+#  siggen_data *= normMax / np.amax(siggen_data)
+#
+#  return siggen_data
+
+
+
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
+
+
---
+++ bayes_changepoint/signal_model_mc2.py
@@ -0,0 +1,231 @@
+#!/usr/local/bin/python
+from ROOT import *
+TROOT.gApplication.ExecuteFile("$MGDODIR/Root/LoadMGDOClasses.C")
+TROOT.gApplication.ExecuteFile("$MGDODIR/Majorana/LoadMGDOMJClasses.C")
+import numpy as np
+from scipy import ndimage
+from pymc import DiscreteUniform, Uniform, Normal, HalfNormal, deterministic, exp
+import matplotlib.pyplot as plt
+
+
+
+#for lookup table
+#dt_array = np.load("siggen_lookup.npy")
+
+
+siggen_conf = "P42661C_autogen_final.conf"
+rcIntTimeConstant = 51.5 * CLHEP.ns
+pzCorrTimeConstant = 57.5*CLHEP.us
+gaussianSmoothing = 1.75
+detZ = np.floor(41.5)
+detRad = np.floor(35.41)
+
+
+siggenInst = GATSiggenInstance(siggen_conf)
+rcint = MGWFRCIntegration()
+rcint.SetTimeConstant(rcIntTimeConstant)
+rcdiff = MGWFRCDifferentiation()
+rcdiff.SetTimeConstant(pzCorrTimeConstant)
+
+
+"""
+    Models for ppc response
+    """
+
+doPlots = 0
+
+if doPlots:
+  plt.ion()
+  fig = plt.figure(10)
+
+def createSignalModelSiggen(data, t0_guess, energy_guess):
+
+  switchpoint = DiscreteUniform('switchpoint', lower=t0_guess-10, upper=t0_guess+10)
+#  switchpoint = t0_guess
+
+  noise_sigma = HalfNormal('noise_sigma', tau=sigToTau(.01))
+  exp_sigma = HalfNormal('exp_sigma', tau=sigToTau(.05))
+  
+  radEst = Uniform('radEst', lower=0, upper=detRad)
+  zEst = Uniform('zEst', lower=5, upper=detZ)
+  phiEst = Uniform('phiEst', lower=0, upper=np.pi/4)
+  
+  wfScale = Normal('wfScale', mu=energy_guess, tau=sigToTau(.01*energy_guess))
+  
+  print "switchpoint is %d" % switchpoint
+  print "wfScale is %f" % wfScale
+
+  ############################
+  @deterministic(plot=False, name="test")
+  def uncertainty_model(s=switchpoint, n=noise_sigma, e=exp_sigma):
+    ''' Concatenate Uncertainty sigmas (or taus or whatever) '''
+    out = np.empty(len(data))
+    out[:s] = n
+    out[s:] = e
+    return out
+  
+  ############################
+  @deterministic
+  def tau(eps=uncertainty_model):
+    return np.power(eps, -2)
+  
+  ############################
+  @deterministic(plot=False, name="siggenmodel")
+  def siggen_model(s=switchpoint, rad = radEst, phi=phiEst, z = zEst, e=wfScale):
+    out = np.zeros(len(data))
+    
+    siggen_data = findSiggenWaveform(rad,phi,z)
+    
+    siggen_data *= e
+    
+    out[s:] = siggen_data[0:(len(data) - switchpoint)]
+    
+    if doPlots:
+      plt.figure(fig.number)
+      plt.clf()
+      plt.plot( data  ,color="red" )
+      plt.plot( out  ,color="blue" )
+      value = raw_input('  --> Press q to quit, any other key to continue\n')
+      if value == 'q':
+        exit(1)
+
+    return out
+  
+  ############################
+
+  baseline_observed = Normal("baseline_observed", mu=siggen_model, tau=tau, value=data, observed= True )
+  return locals()
+
+################################################################################################################################
+
+
+def createSignalModelExponential(data):
+  """
+    Toy model that treats the first ~10% of the waveform as an exponential.  Does a good job of finding the start time (t_0)
+    Since I made this as a toy, its super brittle.  Waveform must be normalized
+  """
+  print "Creating model"
+  switchpoint = DiscreteUniform('switchpoint', lower=0, upper=len(data))
+  
+  noise_sigma = HalfNormal('noise_sigma', tau=sigToTau(.01))
+  exp_sigma = HalfNormal('exp_sigma', tau=sigToTau(.05))
+  
+  #Modeling these parameters this way is why wf needs to be normalized
+  exp_rate = Uniform('exp_rate', lower=0, upper=.1)
+  exp_scale = Uniform('exp_scale', lower=0, upper=.1)
+  
+  timestamp = np.arange(0, len(data), dtype=np.float)
+  
+  @deterministic(plot=False, name="test")
+  def uncertainty_model(s=switchpoint, n=noise_sigma, e=exp_sigma):
+    ''' Concatenate Poisson means '''
+    out = np.empty(len(data))
+    out[:s] = n
+    out[s:] = e
+    return out
+  
+  @deterministic
+  def tau(eps=uncertainty_model):
+    return np.power(eps, -2)
+  
+##  @deterministic(plot=False, name="test2")
+##  def adjusted_scale(s=switchpoint, s1=exp_scale):
+##    out = np.empty(len(data))
+##    out[:s] = s1
+##    out[s:] = s1
+##    return out
+#
+#  scale_param = adjusted_scale(switchpoint, exp_scale)
+
+  @deterministic(plot=False)
+  def baseline_model(s=switchpoint, r=exp_rate, scale=exp_scale):
+    out = np.zeros(len(data))
+    out[s:] = scale * ( np.exp(r * (timestamp[s:] - s)) - 1.)
+    
+#    plt.figure(fig.number)
+#    plt.clf()
+#    plt.plot(out ,color="blue" )
+#    plt.plot(data ,color="red" )
+#    value = raw_input('  --> Press q to quit, any other key to continue\n')
+
+    return out
+
+  baseline_observed = Normal("baseline_observed", mu=baseline_model, tau=tau, value=data, observed= True )
+  return locals()
+
+################################################################################################################################
+
+
+
+def createSignalModelLinear(data):
+  """
+    Toy model that treats the first ~10% of the waveform as an exponential.  Does a good job of finding the start time (t_0)
+    Since I made this as a toy, its super brittle.  Waveform must be normalized
+  """
+  print "Creating model"
+  switchpoint = DiscreteUniform('switchpoint', lower=0, upper=len(data))
+  
+  noise_sigma = HalfNormal('noise_sigma', tau=sigToTau(.01))
+  exp_sigma = HalfNormal('exp_sigma', tau=sigToTau(.05))
+  
+  lin_scale = Uniform('lin_scale', lower=0, upper=.01)
+  
+  timestamp = np.arange(0, len(data), dtype=np.float)
+  
+  @deterministic(plot=False, name="test")
+  def uncertainty_model(s=switchpoint, n=noise_sigma, e=exp_sigma):
+    ''' Concatenate Poisson means '''
+    out = np.empty(len(data))
+    out[:s] = n
+    out[s:] = e
+    return out
+  
+  @deterministic
+  def tau(eps=uncertainty_model):
+    return np.power(eps, -2)
+  
+
+  @deterministic(plot=False)
+  def baseline_model(s=switchpoint, scale=lin_scale):
+    out = np.zeros(len(data))
+    out[s:] = scale * (timestamp[s:] - s)
+    
+#    plt.figure(fig.number)
+#    plt.clf()
+#    plt.plot(out ,color="blue" )
+#    plt.plot(data ,color="red" )
+#    value = raw_input('  --> Press q to quit, any other key to continue\n')
+
+    return out
+
+
+  baseline_observed = Normal("baseline_observed", mu=baseline_model, tau=tau, value=data, observed= True )
+  return locals()
+################################################################################################################################
+
+def sigToTau(sig):
+  tau = np.power(sig, -2)
+#  print "tau is %f" % tau
+  return tau
+################################################################################################################################
+
+def findSiggenWaveform(r,phi,z):
+
+  x = r * np.sin(phi)
+  y = r * np.cos(phi)
+
+  hitPosition = TVector3(x, y, z);
+  sigWf = MGTWaveform();
+  calcFlag = siggenInst.CalculateWaveform(hitPosition, sigWf, 1);
+  
+  if calcFlag == -1:
+    return np.ones(8000)
+  
+  rcint.TransformInPlace(sigWf)
+  rcdiff.TransformInPlace(sigWf)
+  siggen_data = sigWf.GetVectorData()
+  siggen_data = ndimage.filters.gaussian_filter1d(siggen_data, gaussianSmoothing)
+  
+  siggen_data /= np.amax(siggen_data)
+
+  return siggen_data
