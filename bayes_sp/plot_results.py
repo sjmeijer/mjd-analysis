a5d19e71dac0ce2bb0fbda3c7c0d742075c69aa3
Ben Shanks <benjamin.shanks@gmail.com>
2016-02-22 10:17:04 -0500

    skim file plotting

--- bayes_sp/plot_results.py
+++ bayes_sp/plot_results.py
@@ -3,20 +3,49 @@ import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+from ROOT import *
+
 def main(argv):
 
-  (energy, spParam, risetime) = np.loadtxt("results.csv", skiprows=0, unpack=True, delimiter=",")
-
+  wf_file = TFile('spParamSkim.root')
+  tree_wf = wf_file.Get('slowpulseParamTree')
+  
+  energy = []
+  spParam = []
+  wp = []
+  
+  for i in xrange(tree_wf.GetEntries()):
+    tree_wf.GetEntry(i)
+    energy.append(tree_wf.energykeV)
+    spParam.append(tree_wf.spParam)
+    wp.append(tree_wf.wpar)
+  
 
   fig = plt.figure(1)
-  plt.scatter(energy, spParam)
+  plt.scatter(energy, spParam, s=2)
   plt.xlabel("Energy [keV]")
   plt.ylabel("Fit Gaussian Sigma [samples]")
+  plt.xlim(0.5,10)
+  plt.ylim(0, np.amax(spParam))
 
-  fig2 = plt.figure(2)
-  plt.scatter(risetime, spParam)
-  plt.xlabel("10-90 risetime [4-6 keV range]")
-  plt.ylabel("Fit Gaussian Sigma [samples]")
+  fig.savefig("bayes.png")
+
+  plt.ylim(0, 100)
+  fig.savefig("bayes_zoom.png")
+
+  fig = plt.figure(2)
+  plt.scatter(energy, wp, s=2)
+  plt.xlabel("Energy [keV]")
+  plt.ylabel("Wpar")
+  plt.xlim(0.5,10)
+  plt.ylim(0, np.amax(wp))
+
+  fig.savefig("wpar.png")
+
+  plt.ylim(0, 5000)
+  fig.savefig("wpar_zoom.png")
+
+
 
   plt.show()
 
