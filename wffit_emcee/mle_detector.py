30e15bb97ca44c2bbc014406fe509cdb86048700
bshanks <bshanks@unc.edu>
2016-07-26 18:50:28 -0400

    global mle approach to detector fits

---
+++ wffit_emcee/mle_detector.py
@@ -0,0 +1,154 @@
+#!/usr/local/bin/python
+import matplotlib
+#matplotlib.use('CocoaAgg')
+import sys, os
+import matplotlib.pyplot as plt
+import scipy.optimize as op
+import numpy as np
+import emcee
+from scipy import signal
+
+import helpers
+from detector_model import *
+from probability_model_hier import *
+
+from timeit import default_timer as timer
+
+def main(argv):
+
+  plt.ion()
+
+  runRange = (13420,13429)
+  channel = 626
+  aeCutVal = 0.01425
+  
+  numThreads=1
+  tempGuess = 100
+  fitSamples = 200
+  numWaveforms = 5
+  
+
+  #Prepare detector
+#  num = [3.64e+09, 1.88e+17, 6.05e+15]
+#  den = [1, 4.03e+07, 5.14e+14, 7.15e+18]
+  num = [1977053898.8128378, 1.88e+17, 6050000000000461.0]
+  den = [1, 39709003.905781068, 514546561393036.69, 7.15e+18]
+
+  system = signal.lti(num, den)
+  
+  gradGuess = 0.05
+  pcRadGuess = 2.4
+
+  #Create a detector model
+  detName = "conf/P42574A_grad%0.3f_pcrad%0.4f.conf" % (gradGuess,pcRadGuess)
+  det =  Detector(detName, temperature=tempGuess, timeStep=1., numSteps=fitSamples*10, tfSystem=system)
+  det.LoadFields("P42574A_fields.npz")
+  init_detector(det)
+  
+  
+  tempIdx = -9
+  gradIdx = -8
+  pcRadIdx = -7
+  
+  wfFileName = "P42574A_%dwaveforms.npz" % numWaveforms
+  if os.path.isfile(wfFileName):
+    data = np.load(wfFileName)
+    r_arr  = data['r_arr']
+    phi_arr = data['phi_arr']
+    z_arr = data['z_arr']
+    scale_arr = data['scale_arr']
+    t0_arr = data['t0_arr']
+    wfs = data['wfs']
+  
+  else:
+    print "No saved waveforms available.  Loading from Data"
+    #get waveforms
+    cut = "trapECal>%f && trapECal<%f && TSCurrent100nsMax/trapECal > %f" %  (1588,1594, aeCutVal)
+    wfs = helpers.GetWaveforms(runRange, channel, numWaveforms, cut)
+
+    #prep holders for each wf-specific param
+    r_arr = np.empty(numWaveforms)
+    phi_arr = np.empty(numWaveforms)
+    z_arr = np.empty(numWaveforms)
+    scale_arr = np.empty(numWaveforms)
+    t0_arr = np.empty(numWaveforms)
+
+    #ML as a start, using each individual wf
+    nll_wf = lambda *args: -lnlike_waveform(*args)
+    
+    start = timer()
+    for (idx,wf) in enumerate(wfs):
+      print "Doing MLE for waveform %d" % idx
+      wf.WindowWaveformTimepoint(fallPercentage=.99)
+      
+      
+      startGuess = [15./10, np.pi/8, 15./10, wf.wfMax/1000, wf.t0Guess]
+      
+      
+      result = op.minimize(nll_wf, startGuess, args=(wf) ,method="Nelder-Mead")
+      r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx] = result["x"]
+    end = timer()
+    print "Elapsed time: " + str(end-start)
+    np.savez(wfFileName, wfs = wfs, r_arr=r_arr, phi_arr = phi_arr, z_arr = z_arr, scale_arr = scale_arr,  t0_arr=t0_arr,  )
+
+
+  init_wfs(wfs)
+
+
+  #plot the wf-wise MLE:
+  if True:
+    fig = plt.figure()
+    simWfArr = np.empty((1,numWaveforms, fitSamples))
+    for (idx,wf) in enumerate(wfs):
+      print "r: %f\nphi %f\nz %f\n e %f\nt0 %f" % (r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx])
+      simWfArr[0,idx,:] = det.GetSimWaveform(r_arr[idx]*10, phi_arr[idx], z_arr[idx]*10, scale_arr[idx]*1000, t0_arr[idx], fitSamples)
+    helpers.plotManyResidual(simWfArr, wfs, fig, residAlpha=1)
+    value = raw_input('  --> Press q to quit, any other key to continue\n')
+
+  if True:
+    print "Starting detector MLE..."
+    detmleFileName = "P42574A_%dwaveforms_detectormle.npz" % numWaveforms
+
+    nll_det = lambda *args: -lnlike_detector_holdtf(*args)
+    
+    mcmc_startguess = np.hstack((tempGuess/100, gradGuess*10,pcRadGuess))
+    wfParams = np.hstack((r_arr[:], phi_arr[:], z_arr[:], scale_arr[:], t0_arr[:],) )
+
+    #fitting only the 3 detector params
+    start = timer()
+    #result = op.basinhopping(nll_det, mcmc_startguess, minimizer_kwargs={  "method":"Nelder-Mead", "tol":0.5, "args": (wfParams)})
+    #result = op.minimize(nll_det, mcmc_startguess, method="Nelder-Mead", tol=10, args=(wfParams))#options={"fatol":0.5, "xatol":5})
+    
+    bounds = [ (.40, 1.20), (det.gradList[0]*10, det.gradList[-1]*19), (det.pcRadList[0], det.pcRadList[-1])   ]
+    result = op.differential_evolution(nll_det, bounds, args=(wfParams), polish=False)
+    end = timer()
+    print "Elapsed time: " + str(end-start)
+    temp, impGrad, pcRad = result["x"][0], result["x"][1], result["x"][2]
+    temp *= 100
+    impGrad /= 10
+
+
+
+    print "MLE temp is %f" % temp
+    print "MLE grad is %f" % impGrad
+    print "MLE pc rad is %f" % pcRad
+    
+    fig2 = plt.figure()
+    det.SetTemperature(temp)
+    det.SetFields(pcRad, impGrad)
+    det.SetTransferFunction(num, den)
+    simWfArr = np.empty((1,numWaveforms, fitSamples))
+    for (idx,wf) in enumerate(wfs):
+      simWfArr[0,idx,:]   = det.GetSimWaveform(r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx], fitSamples)
+    helpers.plotManyResidual(simWfArr, wfs, fig2, residAlpha=1)
+
+    np.savez(detmleFileName, wfs = wfs, r_arr=r_arr, phi_arr = phi_arr, z_arr = z_arr, scale_arr = scale_arr,  t0_arr=t0_arr,  temp=temp, impGrad=impGrad, pcRad=pcRad)
+    value = raw_input('  --> Press q to quit, any other key to continue\n')
+    exit(0)
+
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
+
+
---
+++ wffit_emcee/probability_model_hier.py
@@ -0,0 +1,98 @@
+#!/usr/local/bin/python
+import numpy as np
+import scipy.stats as stats
+import scipy.optimize as op
+'''Fit detector properties given an array of waveforms'''
+
+
+def init_detector( det):
+  global detector
+  detector = det
+  
+def init_wfs(wf_Arr):
+  global wf_arr
+  wf_arr = wf_Arr
+
+
+def lnlike_detector_holdtf(theta, *wfParams):
+  '''assumes the data comes in w/ 10ns sampling period'''
+
+  temp, impGrad, pcRad = np.copy(theta[:3])
+  
+  wfParams = np.array(wfParams)
+  
+  r_arr, phi_arr, z_arr, scale_arr, t0_arr = wfParams[:].reshape((5, len(wf_arr)))
+  
+  temp *= 100
+  impGrad /= 10
+  
+  print ">>>> temp: %0.2f, pcrad %0.6f, impgrad = %0.4f" % (temp, pcRad, impGrad)
+
+  
+  gradList  = detector.gradList
+  pcRadList =  detector.pcRadList
+  
+  #Take care of detector business
+  if pcRad < pcRadList[0] or pcRad > pcRadList[-1]:
+    return -np.inf
+  if impGrad < gradList[0] or impGrad > gradList[-1]:
+    return -np.inf
+  if temp < 40 or temp > 120:
+    return -np.inf
+  
+  detector.SetTemperature(temp)
+  detector.SetFields(pcRad, impGrad)
+
+  totalLike = 0
+  for (idx) in np.arange(r_arr.size):
+  
+    #find a decent minimization of the wf at this position
+    nll_wf = lambda *args: -lnlike_waveform(*args)
+    
+    result = op.minimize(nll_wf, [r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx]], args=wf_arr[idx] ,method="Nelder-Mead", tol=1.)
+    r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx] = result["x"]
+    wf_like = result['fun']
+    
+    print "  >> wf %d (normalized likelihood %0.2f):" % (idx, wf_like/wf_arr[idx].wfLength)
+    print "      r: %0.2f, phi: %0.3f, z: %0.2f, e: %0.2f, t0: %0.2f" % (r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx])
+    
+#    wf_like = lnlike_waveform( [r_arr[wf_idx], phi_arr[wf_idx], z_arr[wf_idx], scale_arr[wf_idx], t0_arr[wf_idx]], wf_arr[wf_idx])
+
+    if not np.isfinite(wf_like):
+      return -np.inf
+    #NORMALIZE FOR WF LENGTH
+    totalLike += wf_like / wf_arr[idx].wfLength
+  
+  print "  >>total likelihood: %0.3f" % totalLike
+
+  return totalLike
+
+
+
+def lnlike_waveform(theta, wf):
+  r, phi, z, scale, t0 = np.copy(theta)
+  
+  r *= 10
+  z *= 10
+  scale *= 1000
+  
+  if scale < 0 or t0 < 0:
+    return -np.inf
+
+  if not detector.IsInDetector(r, phi, z):
+    return -np.inf
+
+  data = wf.windowedWf
+  model_err = wf.baselineRMS
+
+  model = detector.GetSimWaveform(r, phi, z, scale, t0, len(data))
+  
+  if model is None:
+    return -np.inf
+
+  inv_sigma2 = 1.0/(model_err**2)
+
+
+  return -0.5*(np.sum((data-model)**2*inv_sigma2 - np.log(inv_sigma2)))
+
+
