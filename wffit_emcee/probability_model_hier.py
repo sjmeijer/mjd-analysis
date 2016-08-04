f935ebadc47f7e79587439123dfc151fd7c04bfe
Ben Shanks <bshanks@unc.edu>
2016-08-03 21:22:10 -0400

    for fast fitS

--- wffit_emcee/probability_model_hier.py
+++ wffit_emcee/probability_model_hier.py
@@ -16,9 +16,17 @@ def initializeDetector(det):
   detector = det
   detector.ReinitializeDetector()
 
-def init_wfs(wf_Arr):
+def initializeDetectorAndWfs(det, wfs):
+  global detector
+  detector = det
+  detector.ReinitializeDetector()
+
   global wf_arr
-  wf_arr = wf_Arr
+  wf_arr = wfs
+
+#def init_wfs(wf_Arr):
+#  global wf_arr
+#  wf_arr = wf_Arr
 
 def lnlike_detector(theta, *wfParams):
   '''assumes the data comes in w/ 10ns sampling period'''
@@ -80,6 +88,63 @@ def lnlike_detector(theta, *wfParams):
 
   return totalLike
 
+def lnlike_detector_holdtf(theta, *wfParams):
+  '''assumes the data comes in w/ 10ns sampling period'''
+
+  temp, impGrad, pcRad, pcLen= np.copy(theta[:4])
+
+  temp *= 10.
+  impGrad /= 100.
+  print ">>>> temp: %0.2f, impgrad = %0.4f, pcrad = %0.4f, pclen = %0.4f" % (temp, impGrad, pcRad, pcLen, )
+  
+  pool = wfParams[0]
+  wfParams = np.array(wfParams[1:])
+  
+  r_arr, phi_arr, z_arr, scale_arr, t0_arr, smooth_arr = wfParams[:].reshape((6, len(wf_arr)))
+
+  gradList  = detector.gradList
+  pcRadList =  detector.pcRadList
+  pcLenList =  detector.pcLenList
+  
+  #Take care of detector business
+  if pcRad < pcRadList[0] or pcRad > pcRadList[-1]:
+    return -np.inf
+  if pcLen < pcLenList[0] or pcLen > pcLenList[-1]:
+    return -np.inf
+  if impGrad < gradList[0] or impGrad > gradList[-1]:
+    return -np.inf
+  if temp < 40 or temp > 120:
+    return -np.inf
+  
+  totalLike = 0
+
+  a_args = np.arange(r_arr.size)
+
+  args = []
+
+  for idx in a_args:
+    args.append( [r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx], smooth_arr[idx], temp, pcRad, pcLen, impGrad,  wf_arr[idx] ]  )
+
+  results = pool.map(minimize_wf_hold_tf_star, args)
+
+  for (idx, result) in enumerate(results):
+  
+    r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx], smooth_arr[idx] = result["x"]
+    wf_like = -1*result['fun']
+    
+    print "  >> wf %d (normalized likelihood %0.2f):" % (idx, wf_like/wf_arr[idx].wfLength)
+    print "      r: %0.2f, phi: %0.3f, z: %0.2f, e: %0.2f, t0: %0.2f, smooth: %0.2f" % (r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx], smooth_arr[idx])
+    
+    if not np.isfinite(wf_like):
+      return -np.inf
+    #NORMALIZE FOR WF LENGTH
+    totalLike += wf_like / wf_arr[idx].wfLength
+  
+  print "  >>total likelihood: %0.3f" % totalLike
+
+  return totalLike
+
+
 def minimize_waveform_only(r, phi, z, scale, t0, smooth, wf,):
 
   bounds = [ (0,np.floor(detector.detector_radius*10.)/10. /r_mult ), (0, np.pi/4.), (0,np.floor(detector.detector_length*10.)/10./z_mult), (wf.wfMax/2/scale_mult, wf.wfMax*2/scale_mult), (0, 15), (0, 20)  ]
@@ -124,6 +189,8 @@ def minimize_waveform_only_star(a_b):
 
 def lnlike_waveform(theta, wf):
   r, phi, z, scale, t0, smooth = np.copy(theta)
+#  print "  r: %0.2f, phi: %0.3f, z: %0.2f, e: %0.2f, t0: %0.2f, smooth: %0.2f" % (r, phi, z, scale, t0, smooth)
+
   
   r *= r_mult
   z *= z_mult
@@ -168,6 +235,20 @@ def minimize_wf(r, phi, z, scale, t0, smooth, temp, pcRad, pcLen, impGrad, num, 
 
 def minimize_wf_star(a_b):
   return minimize_wf(*a_b)
+
+def minimize_wf_holdtf(r, phi, z, scale, t0, smooth, temp, pcRad, pcLen, impGrad, wf):
+
+  if detector.temperature != temp:
+    detector.SetTemperature(temp)
+  if detector.pcLen != pcLen or detector.pcRad != pcRad or detector.impurityGrad != impGrad:
+    detector.SetFields(pcRad, pcLen, impGrad)
+  
+  result = op.minimize(neg_lnlike_wf, [r, phi, z, scale, t0, smooth], args=(wf) ,method="Powell")
+
+  return result
+
+def minimize_wf_hold_tf_star(a_b):
+  return minimize_wf_holdtf(*a_b)
 
 def IsAllowableStep(f_new, x_new, f_old, x_old):
   temp, impGrad, pcRad, pcLen = (x_new[:4])
