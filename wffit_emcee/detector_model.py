448c38b8a853093db459741cd1db7533b46047f2
Ben Shanks <bshanks@unc.edu>
2016-08-04 21:05:47 -0400

    commented out print statement

--- wffit_emcee/detector_model.py
+++ wffit_emcee/detector_model.py
@@ -263,7 +263,7 @@ class Detector:
 
     sampled_idxs = np.arange(samples_to_fill)*self.data_to_siggen_size_ratio + siggen_start_idx
     
-    print sampled_idxs[0:10]
+#    print sampled_idxs[0:10]
 
     out = np.zeros(outputLength)
     out[switchpoint_ceil:] = siggen_interp_fn(sampled_idxs)
