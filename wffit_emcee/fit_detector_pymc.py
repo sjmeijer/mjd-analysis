cfd7eae181ce45b53f25bc1250c14500f3b758d2
Ben Shanks <benjamin.shanks@gmail.com>
2016-08-16 14:08:33 -0400

    pymc detector fitter

---
+++ wffit_emcee/fit_detector_pymc.py
@@ -0,0 +1,175 @@
+#!/usr/local/bin/python
+import matplotlib
+#matplotlib.use('CocoaAgg')
+import sys, os
+import matplotlib.pyplot as plt
+import scipy.optimize as op
+import numpy as np
+from scipy import signal
+
+import helpers
+from detector_model import *
+from probability_model_hier import *
+
+from pymc3 import *
+import signal_model_hierarchical as sm3
+
+
+def main(argv):
+
+  plt.ion()
+
+  
+  fitSamples = 200
+  
+  #Prepare detector
+  num =  [3478247474.8078203, 1.9351287044375424e+17, 6066014749714584.0]
+  den = [1, 40525756.715025946, 508584795912802.25, 7.0511687850000589e+18]
+  system = signal.lti(num, den)
+  
+  tempGuess = 77.89
+  gradGuess = 0.0483
+  pcRadGuess = 2.591182
+  pcLenGuess = 1.613357
+
+  #Create a detector model
+  detName = "conf/P42574A_grad%0.2f_pcrad%0.2f_pclen%0.2f.conf" % (0.04,2.5, 1.6)
+  det =  Detector(detName, temperature=tempGuess, timeStep=1., numSteps=fitSamples*10, tfSystem=system)
+  det.LoadFields("P42574A_fields_len.npz")
+  det.SetFields(pcRadGuess, pcLenGuess, gradGuess)
+  
+  
+  wfFileName = "P42574A_512waveforms_8risetimeculled.npz"
+  if os.path.isfile(wfFileName):
+    data = np.load(wfFileName)
+    results = data['results']
+    wfs = data['wfs']
+    numWaveforms = wfs.size
+  else:
+    print "No saved waveforms available.  Loading from Data"
+    exit(0)
+
+  #prep holders for each wf-specific param
+  r_arr = np.empty(numWaveforms)
+  phi_arr = np.empty(numWaveforms)
+  z_arr = np.empty(numWaveforms)
+  scale_arr = np.empty(numWaveforms)
+  t0_arr = np.empty(numWaveforms)
+  smooth_arr = np.ones(numWaveforms)*7.
+
+  simWfArr = np.empty((1,numWaveforms, fitSamples))
+
+  args = []
+  for (idx, wf) in enumerate(wfs):
+    wf.WindowWaveformTimepoint(fallPercentage=.99)
+    r_arr[idx], phi_arr[idx], z_arr[:], scale_arr[idx], t0_arr[idx], smooth_arr[idx]  = results[idx]['x']
+    args.append( [r_arr[idx], phi_arr[idx], z_arr[:], scale_arr[idx], wf.t0Guess,  5.,  wfs[idx] ]  )
+
+  if True:
+#    p = Pool(8, initializer=initializeDetector, initargs=[det])
+#    print "performing parallelized initial fit..."
+#    results = p.map(minimize_waveform_only_star, args)
+#    np.savez(wfFileName, wfs = wfs, results=results )
+
+    fig = plt.figure()
+    for (idx,wf) in enumerate(wfs):
+      wf.WindowWaveform(200)
+      r_arr[idx], phi_arr[idx], z_arr[:], scale_arr[idx], t0_arr[idx], smooth_arr[idx]  = results[idx]['x']
+      
+      print "WF number %d:" % idx
+      print "  >>r: %f\n  >>phi %f\n  >>z %f\n  >>e %f\n  >>t0 %f\n >>smooth %f" % (r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx], t0_arr[idx], smooth_arr[idx])
+      ml_wf = det.GetSimWaveform(r_arr[idx], phi_arr[idx], z_arr[idx], scale_arr[idx]*100, t0_arr[idx], fitSamples, smoothing = smooth_arr[idx])
+      plt.plot(ml_wf, color="b")
+      plt.plot(wf.windowedWf, color="r")
+    value = raw_input('  --> Press q to quit, any other key to continue\n')
+    if value == 'q': exit(0)
+
+  startGuess = {'radEst': r_arr,
+                'zEst': z_arr,
+                'phiEst': phi_arr,
+                'wfScale': scale_arr,
+                'switchpoint': t0_arr,
+                'smooth': smooth_arr,
+                'temp': tempGuess,
+                'grad': gradGuess,
+                'pcRad': pcRadGuess,
+                'pcLen':pcLenGuess}
+
+  siggen_model = sm3.CreateFullDetectorModel(det, wfs,  startGuess, num, den )
+  with siggen_model:
+  
+    step = Metropolis()
+    trace = sample(1000,  step = step)
+    
+    burnin = np.int(len(trace['temp'][:]) - 0.25*len(trace['temp'][:]))#no clue what to choose, for now, just make it 75%
+  
+    temp =  np.median(  trace['temp'][burnin:])
+    print "<<<detector temperature is %f" % temp
+    grad =       np.median(  trace['grad'][burnin:])
+    pcRad=      np.median(  trace['pcRad'][burnin:])
+    pcLen=      np.median(  trace['pcLen'][burnin:])
+    print "<<<detector gradient is %0.4f " % (grad)
+    print "<<<PC Radius is %0.4f, Length is %0.4f " % (pcRad, pcLen)
+    
+    num_1 =      np.median(  trace['num_1'][burnin:])
+    num_2 =      np.median(  trace['num_2'][burnin:])
+    num_3 =      np.median(  trace['num_3'][burnin:])
+    den_1 =      np.median(  trace['den_1'][burnin:])
+    den_2 =      np.median(  trace['den_2'][burnin:])
+    den_3 =      np.median(  trace['den_3'][burnin:])
+
+    print "<<<Detector num is [%0.4e, %0.4e, %0.4e]" % (num_1, num_2, num_3)
+    print "<<<Detector den is [1, %0.4e, %0.4e, %0.4e]" % (den_1, den_2, den_3)
+
+    plt.ioff()
+    traceplot(trace)
+    plt.savefig("hierarchical_full_%dchain.png" % len(wfs))
+    plt.ion()
+##
+##    num = [num_1, num_2, num_3]
+##    den = [1,   den_1, den_2, den_3]
+##    system = signal.lti(num, den)
+##
+#
+#    plt.figure(3)
+#    plt.clf()
+#    plt.title("Charge waveform")
+#    plt.xlabel("Sample number [10s of ns]")
+#    plt.ylabel("Raw ADC Value [Arb]")
+#    
+#    det.tfSystem = system
+#    det.SetTemperature(temp)
+#    det.SetFields(pcRad, grad)
+#    
+    for ( wf_idx, wf) in enumerate(wfs):
+      if len(trace['switchpoint'][:,wf_idx]) < burnin:
+        burnin = 0
+      
+#      t0 =  np.median(  trace['switchpoint'][burnin:,wf_idx])
+#      r =             np.median(  trace['radEst'][burnin:,wf_idx])
+#      z =             np.median(  trace['zEst'][burnin:,wf_idx])
+#      phi =           np.median(  trace['phiEst'][burnin:,wf_idx])
+#      scale =         np.median(  trace['wfScale'][burnin:,wf_idx])
+#      sigma =         np.median(  trace['sigma'][burnin:,wf_idx])
+#      
+#      print "wf number %d" % wf_idx
+#      print "  >> r:   %0.2f" % r
+#      print "  >> z:   %0.2f" % z
+#      print "  >> phi: %0.2f" % phi
+#      print "  >> e:   %0.2f" % scale
+#      print "  >> t0:  %0.2f" % t0
+#
+#      fit_wf = det.GetSimWaveform(r, phi, z, scale, t0, fitSamples)
+#      plt.plot(wf.windowedWf, color="r")
+#      plt.plot(fit_wf, color="b")
+#
+#      
+#    plt.savefig("hierarchical_full_%dwaveforms.png" % len(wfs))
+    summary(trace)
+    value = raw_input('  --> Press q to quit, any other key to continue\n')
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
+
+
---
+++ wffit_emcee/signal_model_hierarchical.py
@@ -0,0 +1,223 @@
+#!/usr/local/bin/python
+
+import numpy as np
+from pymc3 import *#DiscreteUniform, Exponential, Poisson, Uniform, Normal, find_MAP, HalfNormal, switch, Model, Deterministic, exp, Metropolis, traceplot, sample, summary
+import theano.tensor as T
+from theano.compile.ops import as_op
+from scipy import signal
+"""
+    Models for ppc response
+    """
+
+
+
+#def CreateFullDetectorModelGivenTransferFunctionOneField(detector, waveforms, tempGuess):
+#  
+#  n_waveforms = len(waveforms)
+#  sample_length = len(waveforms[0].windowedWf)
+#  
+#  with Model() as signal_model:
+#    
+#    #waveform-specific parameters
+#    radEst = Uniform('radEst', lower=0,   upper=np.floor(detector.detector_radius), shape=n_waveforms)
+#    zEst = Uniform('zEst', lower=0,   upper=np.floor(detector.detector_length), shape=n_waveforms)
+#    phiEst = Uniform('phiEst', lower=0,   upper=np.pi/4, shape=n_waveforms)
+#    
+#    wfScale = Normal('wfScale', mu=waveforms[0].wfMax, sd=.01*waveforms[0].wfMax, shape=n_waveforms)
+#    t0 = Normal('switchpoint', mu=waveforms[0].t0Guess, sd=3, shape=n_waveforms)
+#    
+#    tempEst = Normal('temp', mu=tempGuess, sd=3.)
+#    
+#    @as_op(itypes=[T.dscalar, T.dscalar, T.dscalar, T.dscalar,  T.dscalar, T.dscalar, T.wscalar], otypes=[T.dvector])
+#    def siggen_model(s, rad, phi, z, e, temp, fit_length):
+#      if s<0 or s>= fit_length:
+#        return np.ones(fit_length)*-np.inf
+#     
+#      detector.SetTemperature(temp)
+#      siggen_wf = detector.GetSimWaveform(rad, phi, z, e, s, fit_length)
+#  
+#      if siggen_wf is None:
+#        return np.ones(fit_length)*-np.inf
+#      return siggen_wf
+#        
+#    baseline_observed = []
+#    for (i, wf) in enumerate(waveforms):
+#      wflength = T.as_tensor_variable(np.int16(wf.wfLength))
+#      baseline_observed.append( Normal("baseline_observed_%d" % i, mu=siggen_model(t0[i], radEst[i], phiEst[i], zEst[i], wfScale[i], tempEst, wflength), sd=wf.baselineRMS, observed= wf.windowedWf ) )
+#    return signal_model
+#
+#def CreateFullDetectorModelGivenTransferFunction(detector, waveforms, startGuess):
+#  
+#  n_waveforms = len(waveforms)
+#  gradList = detector.gradList
+#  pcRadList = detector.pcRadList
+#  
+#  print startGuess['radEst']
+#  
+#  
+#  with Model() as signal_model:
+#    
+#    #waveform-specific parameters
+#    radEst = Uniform('radEst', lower=0,   upper=np.floor(detector.detector_radius), shape=n_waveforms, testval = 15.)
+#    zEst = Uniform('zEst', lower=0,   upper=np.floor(detector.detector_length), shape=n_waveforms,  testval = startGuess['zEst'])
+#    phiEst = Uniform('phiEst', lower=0,   upper=np.pi/4, shape=n_waveforms,  testval = startGuess['phiEst'])
+#    
+#    #keep an eye on these, theyre sketchy
+#    wfScale = Normal('wfScale', mu= startGuess['wfScale'], sd=.01*startGuess['wfScale'], shape=n_waveforms, testval=startGuess['wfScale'])
+#    t0 = Normal('switchpoint', mu=startGuess['switchpoint'], sd=3, shape=n_waveforms, testval=startGuess['switchpoint'])
+#    
+#    #detector-wide params
+#    tempEst = Normal('temp', mu= startGuess['temp'], sd=3., testval = startGuess['temp'])
+#    grad = Uniform('grad', lower = gradList[0], upper= gradList[-1], testval=startGuess['grad'])
+#    pcRad = Uniform('pcRad', lower = pcRadList[0], upper= pcRadList[-1], testval=startGuess['pcRad'])
+#    
+#    @as_op(itypes=[T.dscalar, T.dscalar, T.dscalar, T.dscalar,  T.dscalar, T.dscalar, T.dscalar, T.dscalar, T.wscalar], otypes=[T.dvector])
+#    def siggen_model(s, rad, phi, z, e, temp, grad, pcRad, fit_length):
+##      print "grad is %0.4f, pcrad is %0.4f" % (grad, pcRad)
+#
+#      if s<0 or s>= fit_length:
+#        print "cutting off from s at %f" % s
+#        return np.ones(fit_length)*-np.inf
+#      if (grad > gradList[-1]) or (grad <gradList[0]):
+#        print "cutting off from grad at %f" % grad
+#        return np.ones(fit_length)*-np.inf
+#      if (pcRad > pcRadList[-1]) or (pcRad <pcRadList[0]):
+#        print "cutting off from pc rad at %f" % pcRad
+#        return np.ones(fit_length)*-np.inf
+#      if temp < 40 or temp >120:
+#        print "cutting off from temp at %f" % temp
+#        return np.ones(fit_length)*-np.inf
+#          
+#      detector.SetTemperature(temp)
+#      detector.SetFields(pcRad, grad)
+#      siggen_wf = detector.GetSimWaveform(rad, phi, z, e, s, fit_length)
+#  
+#      if siggen_wf is None:
+#        print "siggen wf was none (r:%f, phi:%f, z:%f)" % (rad, phi, z)
+#        return np.ones(fit_length)*-np.inf
+#      return siggen_wf
+#        
+#    baseline_observed = []
+#    for (i, wf) in enumerate(waveforms):
+#      wflength = T.as_tensor_variable(np.int16(wf.wfLength))
+#      baseline_observed.append( Normal("baseline_observed_%d" % i, mu=siggen_model(t0[i], radEst[i], phiEst[i], zEst[i], wfScale[i], tempEst, grad, pcRad, wflength), sd=wf.baselineRMS, observed= wf.windowedWf ) )
+#    return signal_model
+
+def CreateFullDetectorModel(detector, waveforms, startGuess, prior_num, prior_den):
+  
+  n_waveforms = len(waveforms)
+  sample_length = len(waveforms[0].windowedWf)
+  
+  with Model() as signal_model:
+    
+    #waveform-specific parameters
+    radEst = Uniform('radEst', lower=0,   upper=np.floor(detector.detector_radius*10.)/10., shape=n_waveforms, testval=(startGuess['radEst'])  )
+    zEst = Uniform('zEst', lower=0,   upper=np.floor(detector.detector_length*10.)/10., shape=n_waveforms, testval=(startGuess['zEst']))
+    phiEst = Uniform('phiEst', lower=0,   upper=np.pi/4, shape=n_waveforms, testval=(startGuess['phiEst']))
+    
+    wfScale = Normal('wfScale', mu=startGuess['wfScale'], sd=0.05*startGuess['wfScale'], shape=n_waveforms  )
+    
+    BoundAtZero = Bound(Normal, lower=0)
+    t0 =  BoundAtZero('switchpoint', mu=startGuess['switchpoint'], sd=5, shape=n_waveforms)
+    sigma =   BoundAtZero('sigma', mu=startGuess['smooth'], sd=3, shape=n_waveforms)
+    
+    #detector-wide params
+#    tempEst = Bound( Normal('temp', mu=startGuess['temp'], sd=3.), lower=40, upper=120)
+#    grad = Bound( Normal('temp', mu=startGuess['grad'], sd=3.), lower=detector.gradList[0], upper=detector.gradList[-1])
+#    pcRad = Bound( Normal('pcRad', mu=startGuess['pcRad'], sd=0.2), lower=detector.pcRadList[0], upper=detector.pcRadList[-1])
+#    pcLen = Bound( Normal('pcLen', mu=startGuess['pcLen'], sd=0.2), lower=detector.pcLenList[0], upper=detector.pcLenList[-1])
+
+
+    BoundTemp = Bound(Normal, lower=40, upper=120)
+    tempEst = BoundTemp('temp', mu=startGuess['temp'], sd=3.)
+    
+    BoundGrad = Bound(Normal, lower=detector.gradList[0], upper=detector.gradList[-1])
+    BoundRad = Bound(Normal, lower=detector.pcRadList[0], upper=detector.pcRadList[-1])
+    BoundLen = Bound(Normal, lower=detector.pcLenList[0], upper=detector.pcLenList[-1])
+    
+    grad =  BoundGrad('grad', mu=startGuess['grad'], sd=3.)
+    pcRad =  BoundRad('pcRad', mu=startGuess['pcRad'], sd=0.2)
+    pcLen = BoundLen('pcLen', mu=startGuess['pcLen'], sd=0.2)
+
+
+    num_1 = Normal('num_1', mu=prior_num[0], sd=.3*prior_num[0])
+    num_2 = Normal('num_2', mu=prior_num[1], sd=.3*prior_num[1])
+    num_3 = Normal('num_3', mu=prior_num[2], sd=.3*prior_num[2])
+    den_1 = Normal('den_1', mu=prior_den[1], sd=.3*prior_den[1])
+    den_2 = Normal('den_2', mu=prior_den[2], sd=.3*prior_den[2])
+    den_3 = Normal('den_3', mu=prior_den[3], sd=.3*prior_den[3])
+    
+    @as_op(itypes=[T.dscalar, T.dscalar, T.dscalar, T.dscalar, T.dscalar,  T.dscalar, T.dscalar,T.dscalar,  T.dscalar, T.dscalar,T.dscalar,  T.dscalar, T.dscalar, T.dscalar, T.dscalar, T.dscalar, T.wscalar], otypes=[T.dvector])
+    def siggen_model(s, rad, phi, z, e, smooth, temp, num_1, num_2, num_3, den_1, den_2, den_3, grad, pc_rad, pc_len, fit_length):
+    
+      if s<0 or s>= fit_length:
+        return np.ones(fit_length)*-np.inf
+      if smooth<0:
+        return np.ones(fit_length)*-np.inf
+    
+      if (grad > detector.gradList[-1]) or (grad < detector.gradList[0]) :
+        return np.ones(fit_length)*-np.inf
+      if (pc_rad > detector.pcRadList[-1]) or (pc_rad < detector.pcRadList[0]) :
+        return np.ones(fit_length)*-np.inf
+      if (pc_len > detector.pcLenList[-1]) or (pc_len < detector.pcLenList[0]) :
+        return np.ones(fit_length)*-np.inf
+      
+      if not detector.IsInDetector(rad, phi, z):
+        return -np.inf * np.ones(fit_length)
+      
+      num = [num_1, num_2, num_3]
+      den = [1,   den_1, den_2, den_3]
+      detector.SetTransferFunction(num, den)
+      
+      if detector.pcRad != pc_rad or detector.pcLen != pc_len or detector.impurityGrad != grad:
+        detector.SetFields(pc_rad, pc_len, grad)
+      
+      siggen_wf = detector.GetSimWaveform(rad, phi, z, e*100, s, fit_length, smoothing=smooth)
+  
+      if siggen_wf is None:
+#        print "siggen wf is none at (%0.2f, %0.2f, %0.2f)" % (rad, phi, z)
+        return np.ones(fit_length)*-np.inf
+      return siggen_wf
+        
+    baseline_observed = []
+    for (i, wf) in enumerate(waveforms):
+      wflength = T.as_tensor_variable(np.int16(wf.wfLength))
+      baseline_observed.append( Normal("baseline_observed_%d" % i, mu=siggen_model(t0[i], radEst[i], phiEst[i], zEst[i], wfScale[i], sigma[i], tempEst, num_1, num_2, num_3, den_1, den_2, den_3, grad, pcRad, pcLen, wflength), sd=wf.baselineRMS, observed= wf.windowedWf ) )
+    return signal_model
+
+
+################################################################################################################################
+
+class DetectorSignal(Continuous):
+    def __init__(self, detector, *args, **kwargs):
+        super(Beta, self).__init__(*args, **kwargs)
+        self.detector = detector
+
+    def logp(self, value):
+        mu = self.mu
+        return beta_logp(value - mu)
+
+def signal_model(s, rad, phi, z, e, temp, grad, pcRad, fit_length):
+  if s<0 or s>= fit_length:
+    print "cutting off from s at %f" % s
+    return np.ones(fit_length)*-np.inf
+  if (grad > gradList[-1]) or (grad <gradList[0]):
+    print "cutting off from grad at %f" % grad
+    return np.ones(fit_length)*-np.inf
+  if (pcRad > pcRadList[-1]) or (pcRad <pcRadList[0]):
+    print "cutting off from pc rad at %f" % pcRad
+    return np.ones(fit_length)*-np.inf
+  if temp < 40 or temp >120:
+    print "cutting off from temp at %f" % temp
+    return np.ones(fit_length)*-np.inf
+      
+  detector.SetTemperature(temp)
+  detector.SetFields(pcRad, grad)
+  siggen_wf = detector.GetSimWaveform(rad, phi, z, e, s, fit_length)
+
+  if siggen_wf is None:
+    print "siggen wf was none (r:%f, phi:%f, z:%f)" % (rad, phi, z)
+    return np.ones(fit_length)*-np.inf
+  return siggen_wf
+
+
