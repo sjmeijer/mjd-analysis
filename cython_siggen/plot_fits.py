c2394c08a276564450d48ce57a814e33bca1fcb1
Ben Shanks <benjamin.shanks@gmail.com>
2016-10-04 19:07:56 -0400

    better r and z plotting

--- cython_siggen/plot_fits.py
+++ cython_siggen/plot_fits.py
@@ -22,7 +22,7 @@ def main(argv):
   dep_energy = 1597
   binwidth = 0.5
   
-  file_names = ["ms_event_set_runs11510-11530_mcmcfit.npz"]
+  file_names = ["ms_event_set_runs11510-11530_mcmcfit.npz", "ms_event_set_runs11530-11560_mcmcfit.npz", "ms_event_set_runs11560-11570_mcmcfit.npz"]
   all_wfs = []
   for file_name in file_names:
     if os.path.isfile(file_name):
@@ -35,30 +35,34 @@ def main(argv):
   all_wfs = np.concatenate(all_wfs[:])
   energy_arr = np.zeros(all_wfs.size)
   like_arr = np.zeros(all_wfs.size)
-
-  import warnings
-  with warnings.catch_warnings():
-    warnings.filterwarnings('error')
-    for (idx, wf) in enumerate(all_wfs):
-      energy_arr[idx] = wf.energy
-      like_arr[idx] = -1*wf.lnprob / wf.wfLength
+  
+  for (idx, wf) in enumerate(all_wfs):
+    energy_arr[idx] = wf.energy
+    like_arr[idx] = -1*wf.lnprob / wf.wfLength
 
   like_arr[ np.where( np.isnan(like_arr) == 1) ] = np.inf
 
-  good_dep_idxs = np.where(np.logical_and(np.less(energy_arr, 1800), np.less(like_arr, 2)))[0]
+  dep_idxs =  np.where(np.logical_and(np.less(energy_arr, 1800), np.isfinite(like_arr)))[0]
+  r_arr = np.empty(len(dep_idxs))
+  z_arr = np.empty(len(dep_idxs))
+  like_arr_dep = like_arr[dep_idxs]
 
-  r_arr = np.empty(len(good_dep_idxs))
-  z_arr = np.empty(len(good_dep_idxs))
-  
-#  print len(r_arr)
-
-  for (new_idx, all_wf_idx) in enumerate(good_dep_idxs):
+  for (new_idx, all_wf_idx) in enumerate(dep_idxs):
     samples = all_wfs[all_wf_idx].samples
-    r_arr[new_idx] = np.median(samples[:,0])
-    z_arr[new_idx] = np.median(samples[:,2])
+    r_hist, r_bins = np.histogram(samples[:,0], bins=np.linspace(0, 33.8, 339 ))
+    z_hist, z_bins = np.histogram(samples[:,2], bins=np.linspace(0, 39.3, 394 ))
+    
+    r_arr[new_idx] = r_bins[np.argmax(r_hist)]
+    z_arr[new_idx] = z_bins[np.argmax(z_hist)]
+
+  best_dep_idxs = np.where( np.less(like_arr_dep, 2) )[0]
+  ok_dep_idxs = np.where(np.logical_and( np.greater(like_arr_dep, 2),np.less(like_arr_dep, 3) ))[0]
+  bad_dep_idxs = np.where(np.greater(like_arr_dep, 3) )[0]
 
   plt.figure()
-  plt.scatter(r_arr, z_arr)
+  plt.scatter(r_arr[best_dep_idxs], z_arr[best_dep_idxs], color="g")
+  plt.scatter(r_arr[ok_dep_idxs], z_arr[ok_dep_idxs], color="b")
+  plt.scatter(r_arr[bad_dep_idxs], z_arr[bad_dep_idxs], color="r")
 
   plt.xlim(0, 34)
   plt.ylim(0,38)
