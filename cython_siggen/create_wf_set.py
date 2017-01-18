55aa47d4f4221f8df255037ee0c0931dc4941603
Ben Shanks <bshanks@unc.edu>
2017-01-18 13:28:43 -0500

    incremental changes in how i make the set

--- cython_siggen/create_wf_set.py
+++ cython_siggen/create_wf_set.py
@@ -20,16 +20,15 @@ from matplotlib import gridspec
 
 def main(argv):
 
-  numWfsToSave = 16 #must be a mult of 4
+  numWfsToSave = 24 #must be a mult of 4
 
-  runRange = (13420,13429)
   channel = 626
   aeCutVal = 0.01425
-  
+
   fitSamples = 250
-  timeStepSize = 10
-  
-  wfFileName = "fep_and_dep_event_set_runs13385-13557"
+  timeStepSize = 1
+
+  wfFileName = "fep_event_set_runs11531-11539"
 #  wfFileName = "P42574A_512waveforms_30risetimeculled"
   if os.path.isfile(wfFileName + ".npz"):
     data = np.load(wfFileName+ ".npz")
@@ -38,24 +37,27 @@ def main(argv):
   else:
     print "No saved waveforms available.  Loading from Data"
     exit(0)
-  
-  zero_1 = 0.470677
-  pole_1 = 0.999857
-  pole_real = 0.807248
-  pole_imag = 0.085347
-  tempGuess = 78.474793
-  gradGuess = 0.045049
-  pcRadGuess = 2.574859
-  pcLenGuess = 1.524812
-  
-  zeros = [zero_1, -1., 1. ]
-  poles = [pole_1, pole_real+pole_imag*1j, pole_real-pole_imag*1j, ]
+
+  #Prepare detector
+  tempGuess = 79.310080
+  gradGuess = 0.05
+  pcRadGuess = 2.5
+  pcLenGuess = 1.6
 
   #Create a detector model
   detName = "conf/P42574A_grad%0.2f_pcrad%0.2f_pclen%0.2f.conf" % (0.05,2.5, 1.65)
-  det =  Detector(detName, temperature=tempGuess, timeStep=timeStepSize, numSteps=fitSamples*10./timeStepSize, poles=poles, zeros=zeros)
+  det =  Detector(detName, temperature=tempGuess, timeStep=timeStepSize, numSteps=fitSamples*10 )
   det.LoadFields("P42574A_fields_v3.npz")
   det.SetFields(pcRadGuess, pcLenGuess, gradGuess)
+
+  b_over_a = 0.107213
+  c = -0.821158
+  d = 0.828957
+  rc1 = 74.4
+  rc2 = 1.79
+  rcfrac = 0.992
+  det.SetTransferFunction(b_over_a, c, d, rc1, rc2, rcfrac)
+
   initializeDetector(det, )
 
   risetimes = []
@@ -65,61 +67,82 @@ def main(argv):
   energy = []
 
   fig = plt.figure(1)
-  
-  for (idx,wf) in enumerate(wfs):
-#    if wf.energy > 1800: continue
 
-    wf.WindowWaveformTimepoint(fallPercentage=.99, rmsMult=10)
+  for (idx,wf) in enumerate(wfs):
+    if wf.energy < 1800: continue
+
+
+    wf.WindowWaveformTimepoint(fallPercentage=.99, rmsMult=2)
+    if wf.wfMax < 6380: continue
+
+
     rt = findTimePointBeforeMax(wf.windowedWf, 0.995) - wf.t0Guess
-    
+
     rt_90 = findTimePointBeforeMax(wf.windowedWf, 0.90) - wf.t0Guess
-    
+
     if rt - rt_90 > 20: continue
-    
+
     if rt > 200: continue
-#    
-    if rt < 86 and rt > 45: continue
-    
+#
+#    if rt < 95 and rt > 45: continue
+
     if rt < 40: continue
 
 #    if np.amax(wf.windowedWf) < 3820: continue
 
     ae = np.amax(np.diff(wf.windowedWf)) / wf.energy
-    
+
     if ae <0.165: continue
+
+
 
     risetimes.append( rt)
     amax.append( ae )
     wfs_saved.append(wf)
     energy.append(wf.energy)
-  
+
     if wf.energy > 1800:
       colorStr = "r"
     else:
       colorStr = "g"
-  
+
     plt.plot(wf.windowedWf)
-  
+
   wfs_saved = np.array(wfs_saved)
   risetimes = np.array(risetimes)
-  print  "found %d fast wfs" % len(np.where(risetimes < 60)[0])
-  print  "found %d slow wfs" % len(np.where(risetimes > 60)[0])
-  
-  
-  
+  print  "found %d fast wfs" % len(np.where(risetimes < 45)[0])
+  print  "found %d slow wfs" % len(np.where(risetimes > 92)[0])
+
 #  plt.figure()
 #  plt.hist(amax)
 #  plt.show()
 #  exit()
 
-  wfs_fast_hi =  wfs_saved[np.logical_and(np.less(risetimes, 50), np.greater(energy, 1800) ) ][:numWfsToSave/4]
-  wfs_slow_hi =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.greater(energy, 1800))][:numWfsToSave/4]
-  wfs_fast_lo =  wfs_saved[np.logical_and(np.less(risetimes, 50), np.less(energy, 1800) ) ][:numWfsToSave/4]
-  wfs_slow_lo =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.less(energy, 1800))][:numWfsToSave/4]
+#  np.savez("fep_old_postpsa.npz", wfs = wfs_saved )
+#  exit(0)
 
-  wfs_to_save = np.concatenate((wfs_fast_hi, wfs_slow_hi, wfs_fast_lo, wfs_slow_lo))
+  wfs_fast_hi =  wfs_saved[np.logical_and(np.less(risetimes, 45), np.greater(energy, 1800) ) ][:4]
+  wfs_mid_hi =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.less(risetimes, 60))][:4]
+  wfs_mid_hi2 =  wfs_saved[np.logical_and(np.greater(risetimes, 60), np.less(risetimes, 70))][:4]
+  wfs_mid_hi3 =  wfs_saved[np.logical_and(np.greater(risetimes, 70), np.less(risetimes, 80))][:4]
+  wfs_mid_hi4 =  wfs_saved[np.logical_and(np.greater(risetimes, 80), np.less(risetimes, 90))][:4]
+  wfs_slow_hi =  wfs_saved[np.logical_and(np.greater(risetimes, 90), np.greater(energy, 1800))][:4]
+#  wfs_fast_lo =  wfs_saved[np.logical_and(np.less(risetimes, 50), np.less(energy, 1800) ) ][:numWfsToSave/4]
+#  wfs_slow_lo =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.less(energy, 1800))][:numWfsToSave/4]
+
+#  wfs_to_save = wfs_fast_hi
+
+
+  wfs_to_save = np.concatenate((wfs_fast_hi, wfs_slow_hi,  wfs_mid_hi, wfs_mid_hi2, wfs_mid_hi3, wfs_mid_hi4))#, wfs_fast_lo, wfs_slow_lo))
   results = np.empty_like(wfs_to_save)
 
+  plt.figure()
+  for wf in wfs_to_save:
+      plt.plot(wf.windowedWf)
+  plt.show()
+  exit()
+
+  plt.ion()
   fig1 = plt.figure(2)
   plt.clf()
   gs = gridspec.GridSpec(2, 1, height_ratios=[3, 2])
@@ -128,35 +151,38 @@ def main(argv):
   ax1.set_xlabel("Digitizer Time [ns]")
   ax0.set_ylabel("Voltage [Arb.]")
   ax1.set_ylabel("Residual")
-
   for (idx,wf) in enumerate(wfs_to_save):
+
     initializeWaveform(wf)
-    startGuess = [15., np.pi/8, 15., wf.wfMax, wf.t0Guess, 1]
-    result = op.minimize(nll, startGuess,   method="Powell")
+    minresult = None
+    minlike = np.inf
 
-    r, phi, z, scale, t0, smoove = result["x"]
-    r_new = np.amin( [z, np.floor(det.detector_radius)] )
-    z_new = np.amin( [r, np.floor(det.detector_length)] )
-    
-    result2 = op.minimize(nll, [r_new, phi, z_new, scale, wf.t0Guess, 1],  method="Powell")
-    r, phi, z, scale, t0, smoove = result2["x"]
+    for r in np.linspace(4, np.floor(det.detector_radius)-3, 4):
+      for z in np.linspace(4, np.floor(det.detector_length)-3, 4):
+#        for t0 in np.linspace(wf.t0Guess-5, wf.t0Guess+5, 3):
+          if not det.IsInDetector(r,0,z): continue
+          startGuess = [r, np.pi/8, z, wf.wfMax, wf.t0Guess-5, 10]
+          result = op.minimize(nll, startGuess,   method="Nelder-Mead")
 
-    if result2['fun'] < result['fun']:
-      best_result = result2
-    else:
-      best_result = result
+          if result['fun'] < minlike:
+            minlike = result['fun']
+            minresult = result
 
-    results[idx] = best_result
-      
-    r, phi, z, scale, t0, smoove= best_result["x"]
-    ml_wf = det.MakeSimWaveform(r, phi, z, scale, t0, fitSamples,  h_smoothing=smoove)
-    
+    r, phi, z, scale, t0,smooth  = minresult["x"]
+
+
+    results[idx] = minresult
+
+#    r, phi, z, scale, t0,smooth = best_result["x"]
+#    r_det = np.cos(theta) * r
+#    z_det = np.sin(theta) * r
+    ml_wf = det.MakeSimWaveform(r, phi, z, scale, t0, fitSamples,  h_smoothing=smooth)
+
     print "wf number %d" % idx
     print "  energy: %f" % wf.energy
-    print "  smoove: %f" % smoove
+#    print "  smoove: %f" % smoove
     print "  pos:    (%0.2f, %0.2f, %0.2f)" % (r, phi, z)
-    
-    
+
     if wf.energy > 1800:
       colorStr = "r"
     else:
@@ -168,13 +194,13 @@ def main(argv):
 
     ax1.plot(ml_wf[:dataLen] -  wf.windowedWf, color=colorStr)
 
+#
 
-
-
-  wfFileName = "P42574A_%d_fastandslow.npz" % numWfsToSave
+  wfFileName = "P42574A_24_spread.npz"
 
   np.savez(wfFileName, wfs = wfs_to_save, results=results  )
 
+  value = raw_input('  --> Press q to quit, any other key to continue\n')
   plt.show()
 
 
@@ -188,7 +214,7 @@ def findTimePointBeforeMax(data, percent):
   int_data = np.copy(data)
   max_idx = np.argmax(int_data)
   int_data /= int_data[max_idx]
-  
+
   int_data = int_data[0:max_idx]
 
   return np.where(np.less(int_data, percent))[0][-1]
@@ -197,5 +223,3 @@ def findTimePointBeforeMax(data, percent):
 
 if __name__=="__main__":
     main(sys.argv[1:])
-
-
