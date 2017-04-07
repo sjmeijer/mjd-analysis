5f195d60d4d189b95ec669f25c551e867fd78d35
bshanks <bshanks@unc.edu>
2017-04-07 09:36:30 -0400

    culls wfs for training sets

--- cython_siggen/create_wf_set.py
+++ cython_siggen/create_wf_set.py
@@ -1,64 +1,36 @@
 #!/usr/local/bin/python
 import matplotlib
 #matplotlib.use('CocoaAgg')
-import sys, os, time
-import scipy.optimize as op
+import sys, os
 import numpy as np
-import emcee
-from scipy import signal
 
 import helpers
-from detector_model import *
-from probability_model_waveform import *
-
-from progressbar import ProgressBar, Percentage, Bar, ETA
-from multiprocessing import Pool
-from timeit import default_timer as timer
+# from pysiggen import Detector
+# from probability_model_waveform import *
 
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
 def main(argv):
 
-  numWfsToSave = 24 #must be a mult of 4
+  numBins = 6
+  numWfsToSavePerBin = 4
 
+  # channel = 690
+  # wfFileName = "fep_event_set_runs11510-11539_channel690"
+  # save_file_name = "P42662A_%d_spread.npz" % (numBins*numWfsToSavePerBin)
   channel = 626
-  aeCutVal = 0.01425
+  wfFileName = "fep_event_set_runs11510-11539_channel%d" % channel
+  save_file_name = "P42574A_%d_spread.npz" % (numBins*numWfsToSavePerBin)
+  baseline_value = 107
 
-  fitSamples = 250
-  timeStepSize = 1
-
-  wfFileName = "fep_event_set_runs11531-11539"
-#  wfFileName = "P42574A_512waveforms_30risetimeculled"
   if os.path.isfile(wfFileName + ".npz"):
     data = np.load(wfFileName+ ".npz")
     wfs = data['wfs']
     numWaveforms = wfs.size
   else:
-    print "No saved waveforms available.  Loading from Data"
+    print "No saved waveforms available... exiting."
     exit(0)
-
-  #Prepare detector
-  tempGuess = 79.310080
-  gradGuess = 0.05
-  pcRadGuess = 2.5
-  pcLenGuess = 1.6
-
-  #Create a detector model
-  detName = "conf/P42574A_grad%0.2f_pcrad%0.2f_pclen%0.2f.conf" % (0.05,2.5, 1.65)
-  det =  Detector(detName, temperature=tempGuess, timeStep=timeStepSize, numSteps=fitSamples*10 )
-  det.LoadFields("P42574A_fields_v3.npz")
-  det.SetFields(pcRadGuess, pcLenGuess, gradGuess)
-
-  b_over_a = 0.107213
-  c = -0.821158
-  d = 0.828957
-  rc1 = 74.4
-  rc2 = 1.79
-  rcfrac = 0.992
-  det.SetTransferFunction(b_over_a, c, d, rc1, rc2, rcfrac)
-
-  initializeDetector(det, )
 
   risetimes = []
   amax = []
@@ -70,34 +42,31 @@ def main(argv):
 
   for (idx,wf) in enumerate(wfs):
     if wf.energy < 1800: continue
-
+    if wf.baselineMean < baseline_value: continue
 
     wf.WindowWaveformTimepoint(fallPercentage=.99, rmsMult=2)
     if wf.wfMax < 6380: continue
 
-
     rt = findTimePointBeforeMax(wf.windowedWf, 0.995) - wf.t0Guess
-
     rt_90 = findTimePointBeforeMax(wf.windowedWf, 0.90) - wf.t0Guess
+    rt_50 = findTimePointBeforeMax(wf.windowedWf, 0.50) - wf.t0Guess
+    amax.append( rt - rt_50 )
 
-    if rt - rt_90 > 20: continue
-
-    if rt > 200: continue
-#
-#    if rt < 95 and rt > 45: continue
-
+    if rt - rt_50 > 20: continue
     if rt < 40: continue
-
-#    if np.amax(wf.windowedWf) < 3820: continue
-
-    ae = np.amax(np.diff(wf.windowedWf)) / wf.energy
-
-    if ae <0.165: continue
-
-
+#
+#     if rt > 200: continue
+# #
+# #    if rt < 95 and rt > 45: continue
+#
+#     if rt < 40: continue
+#
+# #    if np.amax(wf.windowedWf) < 3820: continue
+#
+#    ae = np.amax(np.diff(wf.windowedWf)) / wf.energy
+#     if ae <0.165: continue
 
     risetimes.append( rt)
-    amax.append( ae )
     wfs_saved.append(wf)
     energy.append(wf.energy)
 
@@ -110,103 +79,34 @@ def main(argv):
 
   wfs_saved = np.array(wfs_saved)
   risetimes = np.array(risetimes)
-  print  "found %d fast wfs" % len(np.where(risetimes < 45)[0])
-  print  "found %d slow wfs" % len(np.where(risetimes > 92)[0])
 
-#  plt.figure()
-#  plt.hist(amax)
-#  plt.show()
-#  exit()
+  print  "found %d OK looking wfs..." % len(wfs_saved)
+  print "generating a set of %d waveforms" % (numWfsToSavePerBin*numBins)
 
-#  np.savez("fep_old_postpsa.npz", wfs = wfs_saved )
-#  exit(0)
+  # plt.figure()
+  # plt.hist(risetimes, bins=50)
+  # plt.show()
+  # exit()
 
-  wfs_fast_hi =  wfs_saved[np.logical_and(np.less(risetimes, 45), np.greater(energy, 1800) ) ][:4]
-  wfs_mid_hi =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.less(risetimes, 60))][:4]
-  wfs_mid_hi2 =  wfs_saved[np.logical_and(np.greater(risetimes, 60), np.less(risetimes, 70))][:4]
-  wfs_mid_hi3 =  wfs_saved[np.logical_and(np.greater(risetimes, 70), np.less(risetimes, 80))][:4]
-  wfs_mid_hi4 =  wfs_saved[np.logical_and(np.greater(risetimes, 80), np.less(risetimes, 90))][:4]
-  wfs_slow_hi =  wfs_saved[np.logical_and(np.greater(risetimes, 90), np.greater(energy, 1800))][:4]
-#  wfs_fast_lo =  wfs_saved[np.logical_and(np.less(risetimes, 50), np.less(energy, 1800) ) ][:numWfsToSave/4]
-#  wfs_slow_lo =  wfs_saved[np.logical_and(np.greater(risetimes, 50), np.less(energy, 1800))][:numWfsToSave/4]
-
-#  wfs_to_save = wfs_fast_hi
-
-
-  wfs_to_save = np.concatenate((wfs_fast_hi, wfs_slow_hi,  wfs_mid_hi, wfs_mid_hi2, wfs_mid_hi3, wfs_mid_hi4))#, wfs_fast_lo, wfs_slow_lo))
-  results = np.empty_like(wfs_to_save)
+  (n,b) = np.histogram(risetimes, bins=numBins)
+  wfs_to_save = np.empty(numWfsToSavePerBin*numBins, dtype=np.object)
+  for i in range(len(n)):
+      if n[i] < numWfsToSavePerBin:
+          print "not enough waveforms per bin to save!"
+          exit(0)
+      else:
+          idxs = np.logical_and(np.less(risetimes, b[i+1]), np.greater(risetimes, b[i]) )
+        #   print wfs_saved[ idxs ][:numWfsToSavePerBin]
+          wfs_to_save[i*numWfsToSavePerBin:i*numWfsToSavePerBin+numWfsToSavePerBin] = wfs_saved[ idxs ][:numWfsToSavePerBin]
 
   plt.figure()
   for wf in wfs_to_save:
       plt.plot(wf.windowedWf)
   plt.show()
-  exit()
 
-  plt.ion()
-  fig1 = plt.figure(2)
-  plt.clf()
-  gs = gridspec.GridSpec(2, 1, height_ratios=[3, 2])
-  ax0 = plt.subplot(gs[0])
-  ax1 = plt.subplot(gs[1], sharex=ax0)
-  ax1.set_xlabel("Digitizer Time [ns]")
-  ax0.set_ylabel("Voltage [Arb.]")
-  ax1.set_ylabel("Residual")
-  for (idx,wf) in enumerate(wfs_to_save):
+  np.savez(save_file_name, wfs = wfs_to_save )
 
-    initializeWaveform(wf)
-    minresult = None
-    minlike = np.inf
-
-    for r in np.linspace(4, np.floor(det.detector_radius)-3, 4):
-      for z in np.linspace(4, np.floor(det.detector_length)-3, 4):
-#        for t0 in np.linspace(wf.t0Guess-5, wf.t0Guess+5, 3):
-          if not det.IsInDetector(r,0,z): continue
-          startGuess = [r, np.pi/8, z, wf.wfMax, wf.t0Guess-5, 10]
-          result = op.minimize(nll, startGuess,   method="Nelder-Mead")
-
-          if result['fun'] < minlike:
-            minlike = result['fun']
-            minresult = result
-
-    r, phi, z, scale, t0,smooth  = minresult["x"]
-
-
-    results[idx] = minresult
-
-#    r, phi, z, scale, t0,smooth = best_result["x"]
-#    r_det = np.cos(theta) * r
-#    z_det = np.sin(theta) * r
-    ml_wf = det.MakeSimWaveform(r, phi, z, scale, t0, fitSamples,  h_smoothing=smooth)
-
-    print "wf number %d" % idx
-    print "  energy: %f" % wf.energy
-#    print "  smoove: %f" % smoove
-    print "  pos:    (%0.2f, %0.2f, %0.2f)" % (r, phi, z)
-
-    if wf.energy > 1800:
-      colorStr = "r"
-    else:
-      colorStr = "g"
-#
-    dataLen = wf.wfLength
-    ax0.plot(ml_wf[:dataLen], color="black")
-    ax0.plot( wf.windowedWf, color=colorStr)
-
-    ax1.plot(ml_wf[:dataLen] -  wf.windowedWf, color=colorStr)
-
-#
-
-  wfFileName = "P42574A_24_spread.npz"
-
-  np.savez(wfFileName, wfs = wfs_to_save, results=results  )
-
-  value = raw_input('  --> Press q to quit, any other key to continue\n')
-  plt.show()
-
-
-def nll(*args):
-  return -lnlike_waveform(*args)
-
+  # exit()
 
 def findTimePointBeforeMax(data, percent):
 
--- cython_siggen/save_wf_set.py
+++ cython_siggen/save_wf_set.py
@@ -18,27 +18,29 @@ detectorName = "P3KJR"
 
 def main(argv):
 
-  runRanges = [(11560, 11570)]#11540)]
-  channel = 690
-  
-  
+  runRanges = [(11510, 11539)]#11540)]
+
+  #
+  #channel = 690
+  channel = 626
+
   side_padding = 15
-  
+
   fep_energy = 2621
   sep_energy = 2109
   dep_energy = 1597
-  
+
   ae_cut = rh.getChannelAECut(channel)
-  
+
   energy_cut_fep = "(trapENFCal > %f && trapENFCal < %f)" % (fep_energy-side_padding,fep_energy+side_padding)
   energy_cut_sep = "(trapENFCal > %f && trapENFCal < %f)" % (sep_energy-side_padding,sep_energy+side_padding)
   energy_cut_dep = "(trapENFCal > %f && trapENFCal < %f)" % (dep_energy-side_padding,dep_energy+side_padding)
-  
-  
+
+
 #  total_cut = "( %s || %s) && channel == %d  " % ( energy_cut_dep, energy_cut_sep, channel, )
 #  wfFileName = "ms_event_set_runs%d-%d.npz" % (runRanges[0][0], runRanges[-1][-1])
 
-  total_cut = "( %s ) && channel == %d  " % ( energy_cut_fep, channel, )
+  total_cut = "( %s ) && channel == %d && %s > 1 " % ( energy_cut_fep, channel, ae_cut)
   wfFileName = "fep_event_set_runs%d-%d_channel%d.npz" % (runRanges[0][0], runRanges[-1][-1], channel)
 
 
@@ -48,7 +50,7 @@ def main(argv):
     for runs in runRanges:
       for run in range(runs[0], runs[1]+1):
         runList.append(run)
-  
+
     chainGat = TChain(gatTreeName)
     chainGat.SetDirectory(0)
 
@@ -66,13 +68,13 @@ def main(argv):
 #    chainGat.SetBranchStatus("channel" ,1)
 
     binsPerKev = 2
-  
+
     canvas = TCanvas("canvas")
     pad2 = TPad("pad2","The Superpulse", 0.05,0.05,0.95,0.95)
     gStyle.SetOptStat(0)
     pad2.Draw()
     pad2.cd()
-  
+
     sepHist = TH1F("hSEP","Single Escape Peak",2*side_padding*binsPerKev,sep_energy-side_padding,sep_energy+side_padding);
     chainGat.Project("hSEP", "trapENFCal", "channel == %d && %s" % (channel, energy_cut_sep));
     sepHist.Draw()
@@ -80,8 +82,8 @@ def main(argv):
     print "found %d sep events" %  sepHist.GetEntries()
     value = raw_input('  --> Make sure the sep hist is as you expect ')
     if value == 'q': exit(0)
-    
-    
+
+
     depHist = TH1F("hDEP","Double Escape Peak",2*side_padding*binsPerKev,dep_energy-side_padding,dep_energy+side_padding);
     chainGat.Project("hDEP", "trapENFCal", "channel == %d && %s" % (channel, energy_cut_dep));
     depHist.Draw()
@@ -89,7 +91,7 @@ def main(argv):
     print "found %d dep events" %  depHist.GetEntries()
     value = raw_input('  --> Make sure the hist is as you expect ')
     if value == 'q': exit(0)
-  
+
     fepHist = TH1F("hFEP","Full Energy Peak",2*side_padding*binsPerKev,fep_energy-side_padding,fep_energy+side_padding);
     chainGat.Project("hFEP", "trapENFCal", "channel == %d && %s" % (channel, energy_cut_fep));
     fepHist.Draw()
@@ -97,9 +99,9 @@ def main(argv):
     print "found %d fep events" %  fepHist.GetEntries()
     value = raw_input('  --> Make sure the hist is as you expect ')
     if value == 'q': exit(0)
-  
-  
-  
+
+
+
   if not os.path.isfile(wfFileName):
     wfs = rh.GetWaveforms( runRanges,  channel, np.inf, total_cut,)
   else:
@@ -109,7 +111,7 @@ def main(argv):
   np.savez(wfFileName, wfs = wfs)
 
   print "Found %d waveforms" % wfs.size
-  
+
   exit(0)
 
 
@@ -117,5 +119,3 @@ def main(argv):
 
 if __name__=="__main__":
     main(sys.argv[1:])
-
-
