e9dcdb5564a284ea327d0e6ed6be85777ffd7bab
Ben Shanks <benjamin.shanks@gmail.com>
2017-05-01 22:21:44 -0400

    working version

--- wffit_dns/ParallelFitManager.py
+++ wffit_dns/ParallelFitManager.py
@@ -9,6 +9,12 @@ from Model import Model
 
 from multiprocessing import Pool, cpu_count
 
+def init_parallelization(conf):
+    global model
+    model = Model( conf,)
+def WaveformLogLikeStar(a_b):
+  return model.calc_wf_likelihood(*a_b)
+
 class ParallelFitManager():
     '''Does the fit using one machine's parllel cores'''
 
@@ -22,11 +28,10 @@ class ParallelFitManager():
 
         if num_threads > self.model.num_waveforms: num_threads = self.model.num_waveforms
 
-        self.pool = Pool(num_threads)
+        self.pool = Pool(num_threads, initializer=init_parallelization, initargs=(fit_configuration,))
 
     def calc_likelihood(self, params):
         num_det_params = self.num_det_params
-
 
         wfs_param_arr = params[num_det_params:].reshape((6, self.num_waveforms))
 
@@ -38,16 +43,27 @@ class ParallelFitManager():
         for wf_idx in range(self.num_waveforms):
             my_wf_params = np.copy(wf_params)
             wf_params[num_det_params:] = wfs_param_arr[:,wf_idx]
-            args.append( [my_wf_params, wf_idx)
+            args.append( [my_wf_params, wf_idx])
 
-        results = pool.map(self.WaveformLogLikeStar, args)
-        results = pool.map(WaveformLogLikeStar, args)
+        results = self.pool.map(WaveformLogLikeStar, args)
 
         lnlike = 0
         for result in (results):
             lnlike += result
+            # print (result)
+        return lnlike
 
-        return np.sum(wf_likes)
+    def fit(self, numLevels, directory="",numPerSave=1000,numParticles=5 ):
 
-    def WaveformLogLikeStar(self, a_b):
-      return self.model.calc_wf_likelihood(*a_b)
+      sampler = dnest4.DNest4Sampler(self.model,
+                                     backend=dnest4.backends.CSVBackend(basedir ="./" + directory,
+                                                                        sep=" "))
+
+      # Set up the sampler. The first argument is max_num_levels
+      gen = sampler.sample(max_num_levels=numLevels, num_steps=200000, new_level_interval=10000,
+                            num_per_step=numPerSave, thread_steps=100,
+                            num_particles=numParticles, lam=10, beta=100, seed=1234)
+
+      # Do the sampling (one iteration here = one particle save)
+      for i, sample in enumerate(gen):
+          print("# Saved {k} particles.".format(k=(i+1)))
